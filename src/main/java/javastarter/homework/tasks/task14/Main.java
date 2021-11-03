1efc90a8c73c3ee7d64fe7502400aac20fbdcdf0
Rishad <49418521+eziomaciavelli@users.noreply.github.com>
2021-11-04 02:04:52 +0400

    Add homework: task14

---
+++ src/main/java/javastarter/homework/tasks/task14/Main.java
@@ -0,0 +1,21 @@
+package javastarter.homework.tasks.task14;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Введите слово: ");
+        String s = reader.readLine();
+
+        System.out.print("Введите индекс: ");
+        int num = Integer.parseInt(reader.readLine());
+
+
+        Solution.findSymbol(s,num);
+    }
+}
---
+++ src/main/java/javastarter/homework/tasks/task14/Solution.java
@@ -0,0 +1,8 @@
+package javastarter.homework.tasks.task14;
+
+public class Solution {
+
+    public static void findSymbol(String s, int a) {
+        System.out.println("Индекс " + a + " хранит в себе символ " + "= " + s.charAt(a));
+    }
+}
