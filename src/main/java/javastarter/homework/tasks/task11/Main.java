1514b6f15ac9085caf25daadb0c385e99db7ee8d
Rishad <49418521+eziomaciavelli@users.noreply.github.com>
2021-11-03 13:29:00 +0400

    Add homework: task11

--- src/main/java/javastarter/homework/tasks/task11/Main.java
+++ src/main/java/javastarter/homework/tasks/task11/Main.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
+//foo(1,5,2) -> 1^2=1; 2^2=4; 3^2=9, 4^2=16, 5^2=25
 public class Main {
     public static void main(String[] args) throws IOException {
 
@@ -13,7 +13,7 @@ public class Main {
         System.out.print("Введите число для возведение: ");
         int a = Integer.parseInt(reader.readLine());
 
-        System.out.print("Введите число итерации возведенного числа: ");
+        System.out.print("Введите количество итерации: ");
         int b = Integer.parseInt(reader.readLine());
 
         System.out.print("Возведение в степень: ");
