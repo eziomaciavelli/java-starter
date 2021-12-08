5e49a67076f9cebbe378812686a5c03510a0343e
eziomaciavelli <eziomaciavelli@gmail.com>
2021-12-09 03:01:08 +0400

    Reworked: homework tasks

--- src/main/java/javastarter/homework/tasks/task18/Main.java
+++ src/main/java/javastarter/homework/tasks/task18/Main.java
@@ -1,5 +1,11 @@
 package javastarter.homework.tasks.task18;
 
+
+/*
+Метод принимает 3 параметра, String a, String b, String c.
+если А и Б содержит в себе букву C вернет true else false...
+ */
+
 public class Main {
     public static void main(String[] args) {
 
