14f4515bec9a1f60956aded7226d54b2fe690ffc
eziomaciavelli <eziomaciavelli@gmail.com>
2021-12-09 03:05:17 +0400

    Add homework: task20

---
+++ src/main/java/javastarter/homework/tasks/task20/Main.java
@@ -0,0 +1,9 @@
+package javastarter.homework.tasks.task20;
+
+
+//удалить пробелы в краях...Пример: "    Hi how are you?    " ->  "Hi how are you?"
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
