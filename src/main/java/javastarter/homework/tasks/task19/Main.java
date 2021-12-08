32d85789ab14b33b12581c2fc63eaf235578ae2f
eziomaciavelli <eziomaciavelli@gmail.com>
2021-12-09 03:04:47 +0400

    Add homework: task19

---
+++ src/main/java/javastarter/homework/tasks/task19/Main.java
@@ -0,0 +1,11 @@
+package javastarter.homework.tasks.task19;
+
+/*
+напишите метод в котором маленькие буквы стали большими а большие маленькими...
+пример: HeLLo WorLD > hEllO wORld
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
