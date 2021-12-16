0a4e7b1caa1c56f00a247b7f497d472185e6c3ff
eziomaciavelli <eziomaciavelli@gmail.com>
2021-12-17 02:43:02 +0400

    Add homework: task18

--- src/main/java/javastarter/homework/tasks/task18/Solution.java
+++ src/main/java/javastarter/homework/tasks/task18/Solution.java
@@ -1,9 +1,5 @@
 package javastarter.homework.tasks.task18;
 
-/*
-Метод принимает 3 параметра, String a, String b, String c.
-если А и Б содержит в себе букву C вернет true else false...
- */
 public class Solution {
 
     public static boolean foo(String a, String b, String c) {
