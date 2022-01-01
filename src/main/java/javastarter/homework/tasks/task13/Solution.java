f3ad2f2282a0681315ad56f72a1c62484d38555e
eziomaciavelli <eziomaciavelli@gmail.com>
2022-01-01 23:21:43 +0400

    Add homework: task13

--- src/main/java/javastarter/homework/tasks/task13/Solution.java
+++ src/main/java/javastarter/homework/tasks/task13/Solution.java
@@ -3,7 +3,7 @@ package javastarter.homework.tasks.task13;
 public class Solution {
 
     public static boolean checkNumEquals(int a, int b, int c, int d) {
-        if (a == b || a == c || a == d) {
+        if (a == b || a == c || a == d || b == c || b == d) {
             return true;
         }
         return false;
